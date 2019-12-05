a8913f31233bcf4689c5c8d3fcd1faba3e7404f0
guoxj <xingjun.guo@medcrab.com>
2019-12-05 10:11:40 +0800

    分片广播

---
+++ xxl-job-executor-samples/my-xxl-job/src/main/java/com/leam/myxxljob/MyXxlJobApplication.java
@@ -0,0 +1,13 @@
+package com.leam.myxxljob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyXxlJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyXxlJobApplication.class, args);
+    }
+
+}
---
+++ xxl-job-executor-samples/my-xxl-job/src/test/java/com/leam/myxxljob/MyXxlJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leam.myxxljob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyXxlJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
